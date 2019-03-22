707b44c7990c82eb42a2a994464555d95f4f4f54
Vincent Attali <vincent.attali@epitech.eu>
2019-03-22 16:09:01 +0100

    Pushed error class

---
+++ src/Error.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_arcade_2018
+** File description:
+** Error
+*/
+
+#include "Error.hpp"
+
+Error::Error(const std::string &msg) : _msg(msg)
+{
+}
+
+const char *Error::what() const noexcept
+{
+    return _msg.c_str();
+}
