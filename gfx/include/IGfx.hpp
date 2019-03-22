3ea44de50f7b3b7da2fb922898abf37f49272131
Vincent Attali <vincent.attali@epitech.eu>
2019-03-22 15:09:12 +0100

    Fixed draw prototype

--- gfx/include/IGfx.hpp
+++ gfx/include/IGfx.hpp
@@ -19,7 +19,7 @@ class IGfx {
         virtual void menu() = 0;
         virtual void getEvents() = 0;
         virtual void clear() = 0;
-        virtual void draw(int posX, int posY, const std::string &path, int retX = 0, int retY = 0) = 0;
+        virtual void draw(int posX, int posY, const std::string &path, int retX1 = 0, int retY1 = 0, int retX2 = 0, int retY2 = 0) = 0;
 };
 
 #endif /* !IGFX_HPP_ */
