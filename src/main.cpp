554feb6936583798598f307620721e1ab96b8ec8
Vincent Attali <vincent.attali@epitech.eu>
2019-03-25 15:56:37 +0100

    Added return value to main

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,13 @@ int main(int ac, char **av)
 {
     try {
         if (ac != 2)
-            throw Error("Bad number of arguments\n");
+            throw Error("Bad number of arguments");
         std::string arg = av[1];
         Core *core = new Core();
         core->loadGfx(arg);
+        return (0);
     } catch (Error &e) {
         std::cerr << e.what() << std::endl;
+        return (84);
     }
 }
